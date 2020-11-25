a954a25315a23635daa6b4292cefdeeb6ac1b528
maurosvasconcelos <mauro.analise@hotmail.com>
2020-11-24 23:14:57 -0300

    modificaçoes

--- apirest/src/main/java/com/voluntarios/apirest/dao/VoluntarioJpaDAO.java
+++ apirest/src/main/java/com/voluntarios/apirest/dao/VoluntarioJpaDAO.java
@@ -6,6 +6,10 @@ import javax.persistence.Persistence;
 
 import com.voluntarios.apirest.models.Voluntario;
 
+/**
+ * @author mauro
+ *
+ */
 public class VoluntarioJpaDAO {
 
 	private static VoluntarioJpaDAO instance;
@@ -33,10 +37,9 @@ public class VoluntarioJpaDAO {
 	public void setEntityManager(EntityManager entityManager) {
 		this.entityManager = entityManager;
 	}
-	
-	
+
 	public Voluntario consultaPorId(long id) {
-        return getEntityManager().find(Voluntario.class, id);
-      }
+		return getEntityManager().find(Voluntario.class, id);
+	}
 
 }
